83b004c04763c2f01bd24e358c0037330563a275
Preetam D'Souza <preetamjdsouza@gmail.com>
2015-10-15 21:18:53 -0700

    Update product model (user-visible) to Maru v0.1!

--- full_hammerhead.mk
+++ full_hammerhead.mk
@@ -26,7 +26,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 PRODUCT_NAME := full_hammerhead
 PRODUCT_DEVICE := hammerhead
 PRODUCT_BRAND := Android
-PRODUCT_MODEL := AOSP on HammerHead
+PRODUCT_MODEL := Maru v0.1
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_RESTRICT_VENDOR_FILES := true
 
