6d7ca278510ef06089926a935ef01649d3171aef
Preetam D'Souza <preetamjdsouza@gmail.com>
2016-10-18 11:43:05 -0400

    Add old-apns-conf.xml to allow smooth upgrades without a factory reset
    old-apns-conf.xml is needed from the previous Android version when doing
    a major OTA upgrade while preserving /data. TelephonyProvider uses it to
    upgrade it's APN db while preserving user/carrier added/edited entries.
    
    Without the old APN file, booting the phone after flashing the update
    zip from v0.2.4 to v0.3 results in a barrage of "...com.android.phone
    has stopped." dialogs.
    
    This adds the apns-full-conf.xml from v0.2.4 for successful upgrades to
    v0.3 without a factory reset.
    
    Signed-off-by: Preetam D'Souza <preetamjdsouza@gmail.com>

--- full_hammerhead.mk
+++ full_hammerhead.mk
@@ -19,6 +19,7 @@
 
 # Get the long list of APNs
 PRODUCT_COPY_FILES := device/lge/hammerhead/apns-full-conf.xml:system/etc/apns-conf.xml
+PRODUCT_COPY_FILES += device/lge/hammerhead/old-apns-conf.xml:system/etc/old-apns-conf.xml
 
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
